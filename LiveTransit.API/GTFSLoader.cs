7ac05049740e3fee6a4e2eff036b214b769ad05b
Mikołaj Godzicki <mikolajgodzicki@gmail.com>
2024-06-08 15:02:30 +0200

    Update

---
+++ LiveTransit.API/GTFSLoader.cs
@@ -0,0 +1,25 @@
+﻿using GTFS;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveTransit.API {
+    public class GTFSLoader {
+        /// <summary>
+        /// Data about every route etc.
+        /// </summary>
+        private GTFSFeed feed;
+
+        public GTFSLoader() {
+            feed = GetFeed(GetPathToData());
+        }
+
+        private string GetPathToData() => Directory.GetCurrentDirectory() + "\\Data";
+
+        private GTFSFeed GetFeed(string path) => new GTFSReader<GTFSFeed>().Read(path);
+
+    }
+}
