3f36db87fc13258fa70e4caa234f35f6cb3563db
Mikołaj Godzicki <mikolajgodzicki@gmail.com>
2024-06-09 02:02:24 +0200

    Updejcik

--- LiveTransit.API/Program.cs
+++ LiveTransit.API/Program.cs
@@ -6,10 +6,7 @@ namespace LiveTransit.API {
 		public static void Main(string[] args) {
 			var builder = WebApplication.CreateBuilder(args);
 
-			// Add services to the container.
-
 			builder.Services.AddControllers();
-			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 			builder.Services.AddEndpointsApiExplorer();
 			builder.Services.AddSwaggerGen();
 
@@ -17,7 +14,6 @@ namespace LiveTransit.API {
 
 			var app = builder.Build();
 
-			// Configure the HTTP request pipeline.
 			if (app.Environment.IsDevelopment()) {
 				app.UseSwagger();
 				app.UseSwaggerUI();
--- LiveTransit/Program.cs
+++ LiveTransit/Program.cs
@@ -6,7 +6,6 @@ namespace LiveTransit {
         public static void Main(string[] args) {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
             builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents();
 
@@ -18,10 +17,8 @@ namespace LiveTransit {
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment()) {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
